3358a008d973d93a8892ca240b9a5e737b04854a
khushiii21 <91782475+khushiii21@users.noreply.github.com>
2021-10-02 15:55:53 +0530

    Create snakegame2.py

---
+++ snakegame2.py
@@ -0,0 +1,19 @@
+# assigning key directions
+def goup():
+    if head.direction != "down":
+        head.direction = "up"
+  
+  
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+  
+  
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+  
+  
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
